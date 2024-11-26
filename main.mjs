02c53824fc2e74110a0b10096f2550b1f3cafedf
jonorl <39122230+jonorl@users.noreply.github.com>
2024-11-26 17:02:47 +0300

    Wrote the skeletons of the next 11 methods

--- main.mjs
+++ main.mjs
@@ -19,17 +19,22 @@ class Tree {
 
   // Methods
 
+  // Auxiliary method to sort and deduplicate values
   sortAndDedupe(array){
+    let n = arr.length;
+
+    if (n === 0) return null;
     let uniqueAndSorted = [...new Set(array)].sort(function (a, b) { return a*1 - b*1; })
     uniqueAndSorted = uniqueAndSorted.join(" ").trim().split(' ');
     uniqueAndSorted = uniqueAndSorted.map(Number);
     return uniqueAndSorted
   }
 
+  // Write a function that takes an array of data and turns it into a balanced binary tree 
+  // full of Node objects appropriately placed 
   buildTree(arr) {
-    let n = arr.length;
-
-    if (n === 0) return null;
+    // sort and deduplicate the array using auxiliary method
+    arr = this.sortAndDedupe(arr);
 
     // Create the root node
     let mid = Math.floor((n - 1) / 2);
@@ -68,13 +73,58 @@ class Tree {
     }
     return root;
   }
+  // Write an insert(value)function that inserts the given value.
+  insert(value){
 
-  preOrder(root){
-    if (root === null)
-        return;
-    console.log(root.data + " ");
-    this.preOrder(root.left);
-    this.preOrder(root.right);
+  }
+  // Write a deleteItem(value)function that deletes the given value.
+  delete(value){
+
+  }
+  // Write a find(value) function that returns the node with the given value.
+  find(value){
+
+  }
+  // Write a levelOrder(callback) function that accepts a callback function as its parameter. 
+  // levelOrder should traverse the tree in breadth-first level order and call the callback 
+  // on each node as it traverses, passing the whole node as an argument, similarly to how 
+  // Array.prototype.forEach might work for arrays.
+  levelOrder(callback){
+
+  } 
+
+  // Write inOrder(callback), preOrder(callback), and postOrder(callback) functions that also 
+  // accept a callback as a parameter. Each of these functions should traverse the tree in their 
+  // respective depth-first order and pass each node to the provided callback. The functions 
+  // should throw an Error if no callback is given as an argument, like with levelOrder.
+  inOrder(callback){
+
+  }
+  preOrder(callback){
+
+  }
+  postOrder(callbal){
+
+  }
+  // Write a height(node) function that returns the given node’s height. Height is defined as 
+  // the number of edges in the longest path from a given node to a leaf node.
+  height(node){
+
+  }
+  // Write a depth(node) function that returns the given node’s depth. Depth is defined as 
+  // the number of edges in the path from a given node to the tree’s root node.
+  depth(node){
+
+  }
+  // Write an isBalanced function that checks if the tree is balanced. A balanced tree 
+  // is one where the difference between heights of the left subtree and the right subtree of 
+  // every node is not more than 1.
+  isBalanced(){
+
+  }
+  // Write a rebalance function that rebalances an unbalanced tree.
+  rebalance(){
+    
   }
 }
 
@@ -82,8 +132,8 @@ const test = new Tree();
 
 let arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 
-let sortedDedupedArray = test.sortAndDedupe(arr)
-let root = test.buildTree(sortedDedupedArray);
+// let sortedDedupedArray = test.sortAndDedupe(arr)
+let root = test.buildTree(arr);
 
 const prettyPrint = (node, prefix = "", isLeft = true) => {
     if (node === null) {
